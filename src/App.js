e48bc6bb506a9998f2680eb6cfc9eb1d5e7b18d8
virtualdharm <mdharm4air.fm@gmail.com>
2024-01-02 02:37:36 +0530

    #3 React Coding - Show/Hide An Element

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-const users = [
-  { name: "John Doe", id: 1 },
-  { name: "Jane Doe", id: 2 },
-  { name: "Billy Doe", id: 3 }
-];
-const UserItems = users.map((x) => <li key={x.id}>{x.name}</li>)
 function App() {
+  const [show, setShow] = React.useState(true);
+
   return (
     <>
-      <h3>User names</h3>
-      <ul>{UserItems}</ul>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Hide Element Below" : "Show Element Below"}
+      </button>
+      {show && <div>Toggle Challenge</div>}
     </>
-  )
+  );
 }
 
 export default App;
