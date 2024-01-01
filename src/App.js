335b32edc78f68aea8fbc3bd7adc11bcb9d0d19d
virtualdharm <mdharm4air.fm@gmail.com>
2024-01-01 20:07:14 +0530

    #2 React Coding - Display Array Of Users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+const users = [
+  { name: "John Doe", id: 1 },
+  { name: "Jane Doe", id: 2 },
+  { name: "Billy Doe", id: 3 }
+];
+const UserItems = users.map((x) => <li key={x.id}>{x.name}</li>)
 function App() {
   return (
-    <div className="App">JSX is cool!</div>
-  );
+    <>
+      <h3>User names</h3>
+      <ul>{UserItems}</ul>
+    </>
+  )
 }
 
 export default App;
