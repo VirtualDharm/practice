f3c533b0548d3eda186545d818c8983c595dee22
virtualdharm <mdharm4air.fm@gmail.com>
2024-01-02 03:38:17 +0530

    #6 React Coding - Update Parent State From Child

---
+++ src/Parent.js
@@ -0,0 +1,31 @@
+import React from 'react'
+function Child({ setValue }) {
+  return (
+    <>
+      <div>Child</div>
+      <button onClick={() => setValue("Parent has been updated!")}>
+        Change Parent Value
+      </button>
+    </>
+  );
+}
+
+function Parent() {
+  const [value, setValue] = React.useState(
+    "I need to be updated from my child"
+  );
+
+  return (
+    <>
+      <h3>Update Parent State Challenge (Using Callback)</h3>
+      <div className="wrapper">
+        <div>Parent</div>
+        <div className="box-wrapper">{value}</div>
+      </div>
+      <div className="wrapper">
+        <Child setValue={setValue} />
+      </div>
+    </>
+  );
+}
+export default Parent;
