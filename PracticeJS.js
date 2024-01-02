9a17ed24152d89ca524d6eff8562b79fa862afe5
virtualdharm <mdharm4air.fm@gmail.com>
2024-01-02 15:03:12 +0530

    Spirally traversing a matrix

--- PracticeJS.js
+++ PracticeJS.js
@@ -51,4 +51,49 @@
 // Arr1=[1, 4, 3, 2]
 // zigZag(Arr1,4)
 // #######################################################
-
+// Spirally traversing a matrix
+// function spirallyTraverse(matrix, r, c) {
+//     const result = [];
+//     let top = 0, bottom = r - 1, left = 0, right = c - 1;
+//     while (top <= bottom && left <= right) {
+//         // Traverse from left to right
+//         for (let i = left; i <= right; i++) {
+//             result.push(matrix[top][i]);
+//         }
+//         top++;
+//         // Traverse from top to bottom
+//         for (let i = top; i <= bottom; i++) {
+//             result.push(matrix[i][right]);
+//         }
+//         right--;
+//         // Traverse from right to left
+//         if (top <= bottom) {
+//             for (let i = right; i >= left; i--) {
+//                 result.push(matrix[bottom][i]);
+//             }
+//             bottom--;
+//         }
+//         // Traverse from bottom to top
+//         if (left <= right) {
+//             for (let i = bottom; i >= top; i--) {
+//                 result.push(matrix[i][left]);
+//             }
+//             left++;
+//         }
+//     }
+//     return result;
+// }
+// const matrix1 = [
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10, 11, 12],
+//     [13, 14, 15, 16]
+// ];
+// console.log(spirallyTraverse(matrix1, 4, 4));
+// const matrix2 = [
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10, 11, 12]
+// ];
+// console.log(spirallyTraverse(matrix2, 3, 4));
+// #######################################################
