1449ba055cea9b307cc9dc38beaafdeab3a6f207
virtualdharm <mdharm4air.fm@gmail.com>
2024-01-05 04:12:23 +0530

    learning queries

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,1 +1,7 @@
 select first_name,last_name from patients where gender = 'M'
+select first_name, last_name from patients where allergies is NULL
+select first_name from patients where first_name like 'C%'
+select first_name, last_name from patients where weight between 100 and 120
+update patients set allergies = 'NKA' where allergies IS NULL
+select first_name || ' ' || last_name as full_name from patients
+SELECT P.first_name,P.last_name,Pr.province_name FROM Patients P JOIN Province_names Pr ON P.province_id = Pr.province_id;SELECT COUNT(*) AS NumberOfPatients FROM Patients WHERE YEAR(birth_date) = 2010
